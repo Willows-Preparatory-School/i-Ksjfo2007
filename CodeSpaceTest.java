a8d552d72fe089c59360f74ce926364ccb01448e
Prayas Durairajan <62479460+Ksjfo2007@users.noreply.github.com>
2023-09-28 03:43:13 +0000

    documented the uses of CodeSpaces in CodeSpaceTest

--- CodeSpaceTest.java
+++ CodeSpaceTest.java
@@ -4,6 +4,9 @@ public class CodeSpaceTest {
         //codespaces still require you to commit and push code
         //basically just an online VM to store my code and editor instead of having to download a java compiler each time
         //will opening this file again in a new codespace change anything? Will i be able to still use the basic java extensions i have downloaded, or will i have to re-set my settings each time?
-        
+        //I have opened the same file in a new codespace... it doesnt seem to save my VScode settings through different codespaces, but it does save codespaces so i can acess them at anytime... i do believe i need internet to make changes, or even acess the codespace.
+        /*codespaces are a good way to edit and work on code with multiple people at once (or if i just want to keep everything in GitHub) but otherwise it makes more sense to use my local VScode application because it allows me to work on things without internet acess.
+        but on the other hand, codespaces does not take up storage spaces... it still makes sense to clone each repository locally before the end of the week so i still have acess to my HW's in the future if i need them. I might use a mixture of codespaces and 
+        cloning my repository locally for HWs and assignments though... it makes more logical sense. */ 
     }
 }

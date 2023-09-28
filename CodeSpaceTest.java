17a3f5b1fe56d7a9ae5aa8fa47006e50650e6141
Prayas Durairajan <62479460+Ksjfo2007@users.noreply.github.com>
2023-09-28 03:38:08 +0000

    documenting my codespace

--- CodeSpaceTest.java
+++ CodeSpaceTest.java
@@ -1,5 +1,9 @@
 public class CodeSpaceTest {
     public static void main(String[] args) {
         System.out.println("Initial CodeSpace Test");
+        //codespaces still require you to commit and push code
+        //basically just an online VM to store my code and editor instead of having to download a java compiler each time
+        //will opening this file again in a new codespace change anything? Will i be able to still use the basic java extensions i have downloaded, or will i have to re-set my settings each time?
+        
     }
 }
